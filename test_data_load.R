df4008a5b1281c53d54714bede4b1dde85d80a0e
Lindsay Lee <lindsay.lee@stjude.org>
2023-05-05 20:16:59 +0000

    Update test script

--- test_data_load.R
+++ test_data_load.R
@@ -1,8 +1,9 @@
 library(dplyr)
 library(dbplyr)
+library(DatabaseConnector)
 
 conn <-
-  connect(
+  DatabaseConnector::connect(
     dbms = "postgresql",
     server = "broadsea-atlasdb/postgres",
     user = "postgres",
@@ -10,12 +11,17 @@ conn <-
     port = 5432
   )
 
+
 #checking important tables:
 
 tbl(conn, in_schema("native", "patients"))
 
+
+tbl(conn, in_schema("cdm_synthea10", "CONCEPT"))
+
+
 tbl(conn, in_schema("cdm_synthea10", "vocabulary"))
-tbl(conn, in_schema("cdm_synthea10", "concept"))
+tbl(conn, in_schema("cdm_synthea10", "CONCEPT"))
 tbl(conn, in_schema("cdm_synthea10", "concept_ancestor"))
 tbl(conn, in_schema("cdm_synthea10", "concept_class"))
 tbl(conn, in_schema("cdm_synthea10", "concept_relationship"))
@@ -23,3 +29,11 @@ tbl(conn, in_schema("cdm_synthea10", "concept_synonym"))
 
 tbl(conn, in_schema("cdm_synthea10", "person"))
 tbl(conn, in_schema("cdm_synthea10", "provider"))
+
+
+#look for SCD concepts
+tbl(conn, in_schema("cdm_synthea10", "CONCEPT")) %>% 
+  filter(concept_name %like% "Sickle%" | 
+           concept_name %like% "sickle%") %>% 
+  select(concept_id, concept_name, domain_id, vocabulary_id, concept_class_id) %>% 
+  print(n = 100)
